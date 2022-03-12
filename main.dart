84585d4293b56ae8ff35d7574ad65a6187d69e57
SM-Zeerak <syedzeerak67@gmail.com>
2022-03-12 23:01:25 +0500

    Ass3

---
+++ main.dart
@@ -0,0 +1,91 @@
+
+import 'dart:io';
+
+void main() {
+   
+   
+   //Question 3
+
+  //  print("Answer 3");
+   
+  //  var lst1 = [1,2,3,4,5,6,7];
+  // var lst2 = [3,5,6,7,9,10];
+  // var set1 = Set.from(lst1);
+  // var set2 = Set.from(lst2);
+  // print(List.from(set1.difference(set2)));
+  
+  //Question 6
+
+  // print("Answer 6");
+
+  // var num = 7;
+  // for (var i = 1; i <= 10; i++) {
+  //   print("$num * $i = ${num * i}");
+  // }
+  
+  //question 7
+  // print ("Answer 7");
+  // var js = "syed1234";
+  // var pass = stdin.readLineSync();
+  // print ("Enter you Password")
+  // if (pass == js){
+  //   print("succesful login");
+  // }
+  // else if (pass != js.isNotEmpty) {
+  //   print("Password Incorrect");
+  // }
+  // else{
+  //   print("Write Your Password");
+
+
+  //Question 8
+    // print("Answer 8");
+    //  List students = ["ALi","Sami","Yousuf"];
+    //  List score = [350,420,180];
+
+    //  var totalmarks = 500;
+    //  var per1 = score[0] / totalmarks * 100;
+    //  var per2 =  score[1] / totalmarks * 100;
+    //  var per3 = score[2] / totalmarks * 100;
+
+    //  List list1 = [students[0],score[0],per1];
+    //  List list2 = [students[1],score[1],per2];
+    //  List list3 = [students[2],score[2],per3];
+
+    //  print(list1);
+    //  print(list2);
+    //  print(list3);
+
+    //Question 10
+
+    // print("Answer 10");
+    // String str = "Hyderabad";
+     
+    // String result = str.replaceAll("Hyder", "Islam");
+     
+    // print(result);
+
+    //Question 11
+
+    // print("Answer 11");
+    // String name = "Muhammad Basit";
+    // String Currentmonth = "February";
+    // double NoUnits = 127.81;
+    // var perunit = 50;
+    // double nap = (NoUnits * perunit);
+    // var lps = nap * 10 /100;
+    // double gap = nap + lps;
+
+    // print("Customer Name = $name");
+    // print("Current Month = $Currentmonth");
+    // print("Number Of Units = $NoUnits");
+    // print("Charges Per Unit = $perunit");
+    // print("Net Amount Pyabale (Within Due Date) = $nap");
+    // print("Late Pyament Surcharge = $lps");
+    // print("Gross Amount Payable (After Due Date) $gap");
+
+   
+
+  
+
+}
